791cca2652abcb4fbebba67a17eea6240120ad59
Ioane-stacks <ioane.natroshvili08b@geolab.edu.ge>
2022-02-08 20:12:45 +0400

    Updates

---
+++ precache-manifest.86c5ed43463eb5e9dc34e0886e7dfb72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb1a6d3c20fc4afb9a432c2b8694654",
+    "url": "/Tours/index.html"
+  },
+  {
+    "revision": "e33195e52788538be549",
+    "url": "/Tours/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/Tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33195e52788538be549",
+    "url": "/Tours/static/js/main.394d6d91.chunk.js"
+  },
+  {
+    "revision": "3128dff8851166541ea2",
+    "url": "/Tours/static/js/runtime-main.83f56e71.js"
+  }
+]);
